a46eb82575ac7ad33394ecc8cf4282e654d39a86
Mikko Tommila <mikko.tommila@nitorcreations.com>
2021-03-11 20:07:09 +0200

    Change high school landing picture

--- src/Highschool_fi.tsx
+++ src/Highschool_fi.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import translate from './translate';
-import logo from './logo.jpg';
+import lotat from './lotat.jpg';
 
 const Highschool_fi = (): JSX.Element => {
   return (
     <div>
       <h1>Ullakon salaisuus</h1>
       <p><strong>Lotan isoisoäidin pimeällä ullakolla on vanha matkalaukku, johon kukaan ei saa koskea. Kenen tuo matkalaukku on, ja mitä siellä on sisällä? Kiivetkää ullakolle Lotta-tytön kanssa, ja selvittäkää yhdessä Ullakon salaisuus luokkanne kanssa. </strong></p>
-      <img src={logo} alt={translate('lotta_foundation')}/>
+      <img className="large" src={lotat} alt={translate('lotta_foundation')}/>
       <p>Lähtekää mukaan selvittämään, mikä Ullakon salaisuus on. Samalla tavoitteena on tutustua ja oppia asioita mm. Suomen historiasta, perinteistä ja kulttuurista erilaisten toiminnallisten tehtävien kautta, joita opettajat voivat yhdessä oppilaiden kanssa tutkia, suunnitella ja toteuttaa kaikkia osallistaen.Tämän tehtävävinkkipaketin ideoinnin tavoitteena on ollut huomioida mahdollisimman paljon laaja-alaisen osaamisen eri osa-alueita.</p>
       <p>“<em>Lottajärjestö oli naisten vapaaehtoinen, aseeton maanpuolustusjärjestö, joka toimi vuosina 1921-1944. Jäseniä järjestössä oli enimmillään noin 240 000, joista 8-17 -vuotiaita pikkulottia lähes 50 000.</em></p>
       <p><em>Rauhan aikana yksi lottajärjestön tärkeimmistä tavoitteista oli tarjota koulutusta jäsenilleen. Koulutuksen avulla lottajärjestö onnistui luomaan organisaation, jolla oli toimintavalmiudet sota-ajan poikkeuksellisissa olosuhteissa. Sota-aikana lottien yhteiskuntavastuu näyttäytyi muun muassa heikompiosaisista huolehtimisena, sotapakolaisten matkan turvaamisena, vanhusten ja lasten auttamisena sekä sairaiden hoitamisena.</em></p>
--- src/Highschool_sv.tsx
+++ src/Highschool_sv.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import translate from './translate';
-import logo from './logo.jpg';
+import lotat from './lotat.jpg';
 
 const Highschool_sv = (): JSX.Element => {
   return (
     <div>
       <h1> Vindens hemlighet </h1>
       <p><strong>Uppe på den mörka vinden hos Lottas mormor finns en gammal kappsäck som ingen får röra. Vems är kappsäcken, och vad finns i den? Klättra tillsammans med din klass upp på vinden i lilla Lottas sällskap och ta reda på vindens hemlighet.</strong></p>
-      <img src={logo} alt={translate ('lotta_foundation')}/>
+      <img className="large" src={lotat} alt={translate ('lotta_foundation')}/>
       <p>Kom med och utforska vindens hemlighet. Målet är att lära känna och bekanta sig med Finlands historia, traditioner och kultur genom olika funktionella uppgifter. Lärare och elever får tillsammans undersöka, planera och genomföra uppgifterna som är planerade så att alla kan delta.</p>
       <p>“<em>Lottarörelsen var en frivillig, vapenlös nationell försvarsorganisation för kvinnor. Den verkade från 1921-1944. Rörelsen hade som mest cirka 240 000 medlemmar, varav nästan 50 000 var 8-17-åringar.</em></p>
       <p><em>Under fredstid var ett av lottarörelsens huvudmål att erbjuda utbildning åt sina medlemmar. Genom utbildningen lyckades lottarörelsen skapa en organisation med stark handlingsförmåga under krigstidens exceptionella omständigheter. Under kriget syntes rörelsens sociala ansvar bland annat i att ta hand om de svagaste i samhället, genom skapa säkra flyktvägar för kringsflyktingar, hjälpa äldre och barn och omsorgen om sjuka.</em></p>
